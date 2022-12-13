d10762d077b7259f69b6e040441b75c9b82e715c
kju2405 <kju2405@khu.ac.kr>
2022-12-13 12:50:44 +0900

    Data exchange between ChickenActivity and SubActivity

--- app/src/main/java/com/example/finalpro/ChickenActivity.java
+++ app/src/main/java/com/example/finalpro/ChickenActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -22,6 +23,7 @@ public class ChickenActivity extends AppCompatActivity {
     private Button btn_menu,btn_review,btn_reservation,btn_waiting,btn_pos;
     private TextView remainSeatsNum;
     int remainSeats=10;
+    private String[] menuList=new String[10];
 
 
     @Override
@@ -99,6 +101,16 @@ public class ChickenActivity extends AppCompatActivity {
     public void mOnClick(View view){
         Intent intent=new Intent(this,SubActivity.class);
         intent.putExtra("remainSeats",remainSeats);
+        intent.putExtra("num1",menuList[0]);
+        intent.putExtra("num2",menuList[1]);
+        intent.putExtra("num3",menuList[2]);
+        intent.putExtra("num4",menuList[3]);
+        intent.putExtra("num5",menuList[4]);
+        intent.putExtra("num6",menuList[5]);
+        intent.putExtra("num7",menuList[6]);
+        intent.putExtra("num8",menuList[7]);
+        intent.putExtra("num9",menuList[8]);
+        intent.putExtra("num10",menuList[9]);
         startActivityForResult(intent,0);
     }
 
@@ -107,6 +119,17 @@ public class ChickenActivity extends AppCompatActivity {
         if(requestCode==0 && resultCode==RESULT_OK){
             remainSeats=data.getIntExtra("result",0);
             remainSeatsNum.setText("잔여좌석 : "+remainSeats+"좌석");
+            menuList[0]=data.getStringExtra("num1");
+            menuList[1]=data.getStringExtra("num2");
+            menuList[2]=data.getStringExtra("num3");
+            menuList[3]=data.getStringExtra("num4");
+            menuList[4]=data.getStringExtra("num5");
+            menuList[5]=data.getStringExtra("num6");
+            menuList[6]=data.getStringExtra("num7");
+            menuList[7]=data.getStringExtra("num8");
+            menuList[8]=data.getStringExtra("num9");
+            menuList[9]=data.getStringExtra("num10");
+            Toast.makeText(this.getApplicationContext(),"잔여좌석 : "+remainSeats,Toast.LENGTH_SHORT).show();
         }
         super.onActivityResult(requestCode,resultCode,data);
     }
--- app/src/main/java/com/example/finalpro/SubActivity.java
+++ app/src/main/java/com/example/finalpro/SubActivity.java
@@ -3,6 +3,7 @@ package com.example.finalpro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +11,102 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SubActivity extends AppCompatActivity {
 
     private int remainSeats;
+    private EditText num1,num2,num3,num4,num5,num6,num7,num8,num9,num10;
+    int remainingSeatsAfterCalc=0;
+    private String[] menuList=new String[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+        num1=findViewById(R.id.num1);
+        num2=findViewById(R.id.num2);
+        num3=findViewById(R.id.num3);
+        num4=findViewById(R.id.num4);
+        num5=findViewById(R.id.num5);
+        num6=findViewById(R.id.num6);
+        num7=findViewById(R.id.num7);
+        num8=findViewById(R.id.num8);
+        num9=findViewById(R.id.num9);
+        num10=findViewById(R.id.num10);
 
         Intent intent=getIntent();
         remainSeats=intent.getIntExtra("remainSeats",-1);
+        menuList[0]=intent.getStringExtra("num1");
+        num1.setText(menuList[0]);
+        menuList[1]=intent.getStringExtra("num2");
+        num2.setText(menuList[1]);
+        menuList[2]=intent.getStringExtra("num3");
+        num3.setText(menuList[2]);
+        menuList[3]=intent.getStringExtra("num4");
+        num4.setText(menuList[3]);
+        menuList[4]=intent.getStringExtra("num5");
+        num5.setText(menuList[4]);
+        menuList[5]=intent.getStringExtra("num6");
+        num6.setText(menuList[5]);
+        menuList[6]=intent.getStringExtra("num7");
+        num7.setText(menuList[6]);
+        menuList[7]=intent.getStringExtra("num8");
+        num8.setText(menuList[7]);
+        menuList[8]=intent.getStringExtra("num9");
+        num9.setText(menuList[8]);
+        menuList[9]=intent.getStringExtra("num10");
+        num10.setText(menuList[9]);
+
         if(remainSeats!=-1){
             Toast.makeText(this.getApplicationContext(),"잔여좌석 : "+remainSeats,Toast.LENGTH_SHORT).show();
         }
     }
 
+    public void checkRemainingSeats(){
+        remainingSeatsAfterCalc=0;
+        if(num1.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num2.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num3.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num4.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num5.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num6.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num7.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num8.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num9.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+        if(num10.getText().toString().matches("")){
+            remainingSeatsAfterCalc++;
+        }
+    }
+
+
     public void mOnClick(View view){
+        checkRemainingSeats();
         Intent data=new Intent();
-        data.putExtra("result",remainSeats);
+        data.putExtra("result",remainingSeatsAfterCalc);
+        data.putExtra("num1",num1.getText().toString());
+        data.putExtra("num2",num2.getText().toString());
+        data.putExtra("num3",num3.getText().toString());
+        data.putExtra("num4",num4.getText().toString());
+        data.putExtra("num5",num5.getText().toString());
+        data.putExtra("num6",num6.getText().toString());
+        data.putExtra("num7",num7.getText().toString());
+        data.putExtra("num8",num8.getText().toString());
+        data.putExtra("num9",num9.getText().toString());
+        data.putExtra("num10",num10.getText().toString());
         setResult(RESULT_OK,data);
         finish();
     }
